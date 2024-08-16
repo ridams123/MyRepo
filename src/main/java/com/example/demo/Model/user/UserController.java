d8b14cd9634126aa35610300cc4d57bbbef898b0
ridam_soni <ridamsoni2@gmail.com>
2024-08-16 14:50:28 +0530

    gg1

---
+++ src/main/java/com/example/demo/Model/user/UserController.java
@@ -0,0 +1,18 @@
+package com.example.demo.Model.user;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+}
